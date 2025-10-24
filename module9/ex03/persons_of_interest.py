537ee242ba56fc9aaf49831db05857f570bddc2c
Mahmut Kilic <mahkilic@f1r4s16.codam.nl>
2025-10-24 15:55:27 +0200

    little update

--- module9/ex03/persons_of_interest.py
+++ module9/ex03/persons_of_interest.py
@@ -5,9 +5,9 @@ def famous_births(dic):
 		print(f"{v['name']} is a great scientist born in {v['date_of_birth']}.")
 
 women_scientists = {
-"ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
-"cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
-"lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
-"grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+	"ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
+	"cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
+	"lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
+	"grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
 }
 famous_births(women_scientists)
